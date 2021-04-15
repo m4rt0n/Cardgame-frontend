64db645f5c1ba571f7f39e3b90d56cf35f79ed6b
Your Name <dombovari.marton@gmail.com>
2021-04-15 22:42:38 +0200

    table with strap tags

--- src/components/List.js
+++ src/components/List.js
@@ -1,54 +1,76 @@
 import axios from "axios";
 import { Component } from "react";
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
 import './List.css';
 
-class List extends Component{
-state={
-   items: [],
-   showDetails: false,
-};
+class List extends Component {
+    state = {
+        items: [],
+    };
 
-componentDidMount(){
-    axios.get('http://localhost:8081/cards/getall/').then(response=>{
-        this.setState({
-            items: response.data,
-        });
-console.log(response)
+    componentDidMount() {
+        axios.get('http://localhost:8081/cards/getall/').then(response => {
+            this.setState({
+                items: response.data,
+            });
+            console.log(response)
         }
-    );
-}
+        );
+    }
+    clickEvent = (x) => {
+        console.log('clicked: ', x);
 
+    }
+    editEvent = (x) => {
+        console.log('edit: ', x);
 
-
-
-    clickEvent=(x)=>{  
-console.log('clicked: ',x );
+    }
+    deleteEvent = (x) => {
+        console.log('delete: ', x);
 
     }
 
-render(){
-    const {showDetails} = this.state;
-    return(
-        <div className = "list">
-    <p>List: </p>
-        {this.state.items.map((item, index)=>(
-            <ul  className = "card" 
-            onClick={()=>this.clickEvent(item.english)}
-             key={index}> Eng: {item.english}  Spanish: {item.spanish}  Picture: {item.picture.title}</ul>
-        ))}
+    render() {
+        const cards = this.state.items;
+        const list = cards.map(card => {
+            return <tr key={card.id}>
+                <td>{card.english}</td>
+                <td>{card.spanish} </td>
+                <td>{card.picture.title}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button onClick={() => this.editEvent(card.english)}>E</Button>
+                        <Button onClick={() => this.deleteEvent(card.english)}>D</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
 
-<div className="details">
-    <p>Details: </p>
-            <button onClick={() => this.setState({ showDetails: !showDetails })}>toggle</button>
-            <div style={{ display: (showDetails ? 'block' : 'none') }}>This is visible</div>
+        return (
+            <div className="list">
+                <Container fluid>
+                <p>List:</p>
+                <Table className="table">
+                    <thead>
+                        <tr>
+                            <th width="20%">English</th>
+                            <th width="20%">Spanish</th>
+                            <th width="10%">Picture</th>
+                            <th width="10%">Buttons</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list}
+                    </tbody>
+                </Table>
+                </Container>
             </div>
-
-        </div>
-        
-        
         )
 
-        }
+
+
+    }
+
 
 };
 export default List
