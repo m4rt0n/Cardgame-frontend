25404021347076e8d6badfa73de4e91f7b5a178b
Your Name <dombovari.marton@gmail.com>
2021-05-04 11:22:48 +0200

    debugging card render

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -5,76 +5,73 @@ import Card from './Card';
 
 
 class Stack extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-      }
-    
 
-    
+    }
+
+
+
     buttonEvent = (x) => {
         console.log('props: ', x);
     }
-    editEvent = (x) => {
-        console.log('edit: ', x);
 
-    }
-    deleteEvent = (x) => {
-        console.log('delete: ', x);
 
-    }
-    addEvent = () => {
-        console.log('add');
-    }
+
+
     render() {
-    if (this.props) {   
-        console.log('props: ',this.props)
+        let itemsToRender = [];
+        if (this.props) {
+            console.log('props: ', this.props)
+            if (this.props.stack) {
+                console.log(this.props.stack)
+                if (this.props.stack.cards) {
+                    console.log(this.props.stack.cards)
+                    {                       
+                        itemsToRender = this.props.stack.cards.map(card => {
+                            return (
+                                <div className="cardList">
+                                    <Card key={card.english}
+                                        english={card.english}
+                                        spanish={card.spanish}
+                                        picture={card.picture}
+                                    />
+                                </div>)
+                        })
+                    }
+                } else { console.log('cards undefined') }
 
-        const stack = this.props.stack;   
-      //     const data = Array.from(stack); 
-        const list = stack.map(card => {
-          return( 
-          <div className="cardList">
-              <Card key={card.english} 
-          english={card.english}
-          spanish={card.spanish}
-          picture={card.picture} 
-                  
-       />
-       </div>)
-        
-         
-         
-        });
-        /*
-        if (!Array.isArray(data) || !data.length) {
-            console.log('data array is empty')
-        }
-*/
-        if(this.props.stack){
-            console.log('stack: ',this.props.stack)
-        }else{
-            console.log('no stack')
-        }
+            } else { console.log('stack undefined') }
 
-        return (
-            <div className="container">
-                <table className="table">
-                    <thead >
-                        <tr >
-                            <th width="10%" >Picture</th>
-                            <th width="20%">English</th>
-                            <th width="20%">Spanish</th>
-                            <th width="10%">Buttons</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-         {list}
-         </tbody>
-                </table>  
-                        
-            </div>   
-        );
-        }else{console.log("no props")}
+
+
+
+            /*
+            if (!Array.isArray(data) || !data.length) {
+                console.log('data array is empty')
+            }
+    */
+            
+
+            return (
+                <div className="container">
+                    <table className="table">
+                        <thead >
+                            <tr >
+                                <th width="10%" >Picture</th>
+                                <th width="20%">English</th>
+                                <th width="20%">Spanish</th>
+                                <th width="10%">Buttons</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {itemsToRender}
+                        </tbody>
+                    </table>
+
+                </div>
+            );
+        } else { console.log("no props") }
     };
 };
 
